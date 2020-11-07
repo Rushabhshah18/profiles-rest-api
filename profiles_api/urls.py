ec152f6ce4a3b794859a778581db4435710e922e
Rushabh Shah <rushabshah130@gmail.com>
2020-11-06 16:56:48 -0800

    "Added Login API using token authentication"

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from profiles_api.views import HelloApiView,HelloViewSet,UserProfileViewSet
+from profiles_api.views import HelloApiView,HelloViewSet,UserProfileViewSet,UserLoginApiView
 
 router = DefaultRouter()
 router.register('hello-viewset',HelloViewSet,base_name='hello-viewset')
 router.register('profile',UserProfileViewSet)
 urlpatterns = [
     path('hello-view/',HelloApiView.as_view()),
+    path('login',UserLoginApiView.as_view()),
     path('',include(router.urls))
 
 ]
